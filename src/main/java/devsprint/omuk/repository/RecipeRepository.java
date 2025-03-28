e5ec5b35dbf98246610ca631904b59115ebfd4f0
junsoo0719 <junsoo0719@gmail.com>
2025-03-29 03:42:46 +0900

    feat: RecipeRepository 생성

--- src/main/java/devsprint/omuk/repository/RecipeRepository.java
+++ src/main/java/devsprint/omuk/repository/RecipeRepository.java
@@ -1,9 +1,17 @@
 package devsprint.omuk.repository;
 
+import devsprint.omuk.domain.MealTime;
 import devsprint.omuk.domain.Recipe;
+import devsprint.omuk.domain.Season;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    List<Recipe> findByTitleContaining(String keyword);
+    List<Recipe> findByMealTimesContaining(MealTime mealTime);
+    List<Recipe> findBySeasonsContaining(Season season);
+    List<Recipe> findByMealTimesContainingAndSeasonsContaining(MealTime mealTime, Season season);
 }
