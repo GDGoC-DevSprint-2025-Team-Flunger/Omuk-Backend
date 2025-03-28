4270b57a2ee0b6a516c4461dcc0b794dd95d3051
junsoo0719 <junsoo0719@gmail.com>
2025-03-28 16:37:49 +0900

    Init: Initial commit

---
+++ src/main/java/devsprint/omuk/OmukApplication.java
@@ -0,0 +1,13 @@
+package devsprint.omuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devsprint/omuk/OmukApplicationTests.java
@@ -0,0 +1,13 @@
+package devsprint.omuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
